ed5edea2b3e2e198143c573dd8e2ce3318f17774
Jinpisuth <164853102+Jinpisuth@users.noreply.github.com>
2024-03-25 16:58:13 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-import altair as alt
-import time
-import zipfile
+
 
 def load_data():
     df = pd.read_csv('Data Greenie All - Sheet 1.csv')
