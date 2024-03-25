42b63ea69799cdbcf59907f6c5333e194118c431
Jinpisuth <164853102+Jinpisuth@users.noreply.github.com>
2024-03-25 17:01:16 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,8 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+#import plotly.express as px
 import numpy as np
 
 st.set_page_config(page_title="ESG Interactive AI Dashboard", page_icon=":earth_asia:",layout="wide")
 st.title('🌏 ESG Interactive AI Dashboard')
-def load_data():
-    df = pd.read_csv('Data Greenie All - Sheet 1.csv')
-    return df
-
-# Load the data
-data = load_data()
-
-st.dataframe(data)
 
