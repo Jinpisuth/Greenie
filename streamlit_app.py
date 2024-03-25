5bf76fe3c201a5cbf01a4cb77758ca2e49c97a89
Jinpisuth <164853102+Jinpisuth@users.noreply.github.com>
2024-03-25 16:32:01 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
 
+def load_data():
+    df = pd.read_csv('Data Greenie All - Sheet 1.csv')
+    return df
+
+# Load the data
+data = load_data()
+
+Data Greenie All - Sheet 1.csv
 st.set_page_config(page_title="ESG Analysis Dashboard", page_icon=":earth_asia:",layout="wide")
 st.title('🌏 ESG Interactive AI Dashboard')
