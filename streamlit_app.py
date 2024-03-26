4fb50837a0a3e33d36c367586d61ddce2d1bce5f
Jinpisuth <164853102+Jinpisuth@users.noreply.github.com>
2024-03-26 15:26:04 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 #import plotly.express as px
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="ESG Interactive AI Dashboard", page_icon=":earth_asia:",layout="wide")
 st.title('🌏 ESG Interactive AI Dashboard')
